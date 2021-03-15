01e0be1ac3e2dcfbaebe64208bb10b81fdc515a2
telltao <telltao@qq.com>
2021-03-15 21:48:51 +0800

    删除了乱码部分

--- src/com/java/view/MainFrm.java
+++ src/com/java/view/MainFrm.java
@@ -44,24 +44,21 @@ public class MainFrm extends JFrame {
 		menu.setIcon(new ImageIcon(MainFrm.class.getResource("/images/bookTypeManager.png")));
 		menuBar.add(menu);
 
-		//?????????
 		JMenuItem menuItem_1 = new JMenuItem("图书类别添加");
 		menuItem_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//??BookType_Add_InterFrm??????????
 				BookType_Add_InterFrm bookType_add_InterFrm=new BookType_Add_InterFrm();
-				bookType_add_InterFrm.setVisible(true);//??????
-				table.add(bookType_add_InterFrm);//??BookType_Add_InterFrm????????????
+				bookType_add_InterFrm.setVisible(true);
+				table.add(bookType_add_InterFrm);
 			}
 		});
 		menuItem_1.setIcon(new ImageIcon(MainFrm.class.getResource("/images/add.png")));
 		menu.add(menuItem_1);
 
-		//?????????
 		JMenuItem menuItem_2 = new JMenuItem("图书类别维护");
 		menuItem_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//??BookType_Manager_InterFrm??????????
+
 				BookType_Manager_InterFrm bookType_Manager_InterFrm=new BookType_Manager_InterFrm();
 				bookType_Manager_InterFrm.setVisible(true);//??????
 				table.add(bookType_Manager_InterFrm);//??BookType_Manager_InterFrm????????????
@@ -74,11 +71,10 @@ public class MainFrm extends JFrame {
 		menu_1.setIcon(new ImageIcon(MainFrm.class.getResource("/images/bookManager.png")));
 		menuBar.add(menu_1);
 
-		//?????????
 		JMenuItem menuItem_3 = new JMenuItem("图书添加");
 		menuItem_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//??Book_Add_InterFrm??????????
+
 				Book_Add_InterFrm book_Add_InterFrm=new Book_Add_InterFrm();
 				book_Add_InterFrm.setVisible(true);//??????
 				table.add(book_Add_InterFrm);//??Book_Add_InterFrm????????????
@@ -87,20 +83,20 @@ public class MainFrm extends JFrame {
 		menuItem_3.setIcon(new ImageIcon(MainFrm.class.getResource("/images/add.png")));
 		menu_1.add(menuItem_3);
 
-		//??????????
+
 		JMenuItem menuItem_4 = new JMenuItem("图书维护");
 		menuItem_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//??Book_Manage_InterFrm??????????
+
 				Book_Manage_InterFrm book_Manage_InterFrm=new Book_Manage_InterFrm();
-				book_Manage_InterFrm.setVisible(true);//??????
-				table.add(book_Manage_InterFrm);//??Book_Manage_InterFrm????????????
+				book_Manage_InterFrm.setVisible(true);
+				table.add(book_Manage_InterFrm);
 			}
 		});
 		menuItem_4.setIcon(new ImageIcon(MainFrm.class.getResource("/images/edit.png")));
 		menu_1.add(menuItem_4);
 
-		//?????? JMenuItem
+		//JMenuItem
 		JMenuItem menuItem = new JMenuItem("安全退出");
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -113,14 +109,14 @@ public class MainFrm extends JFrame {
 		menuItem.setIcon(new ImageIcon(MainFrm.class.getResource("/images/exit.png")));
 		menuBar.add(menuItem);
 
-		//???????? JMenuItem
+		//JMenuItem
 		JMenuItem mntmNewMenuItem = new JMenuItem("关于我们");
 		mntmNewMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//??About_us_InterFrm??????????????????
+				//
 				About_us_InterFrm about=new About_us_InterFrm();
-				about.setVisible(true);//??????
-				table.add(about);//??about???????????????????
+				about.setVisible(true);//
+				table.add(about);//
 			}
 		});
 		mntmNewMenuItem.setIcon(new ImageIcon(MainFrm.class.getResource("/images/about.png")));
@@ -134,10 +130,10 @@ public class MainFrm extends JFrame {
 		table.setBackground(Color.CYAN);
 		contentPane.add(table, BorderLayout.CENTER);
 
-		//????JFrame???????
+		//
 		//this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-		//????JFrame??????????
+		//
 		this.setLocationRelativeTo(null);
 	}
 
