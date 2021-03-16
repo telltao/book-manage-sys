90b88da089cf87d2582c9b6e48513eb9bb98fd6f
telltao <telltao@qq.com>
2021-03-16 23:21:12 +0800

    修改了关于系统的描述部分

--- src/com/java/view/AboutFrame.java
+++ src/com/java/view/AboutFrame.java
@@ -37,7 +37,7 @@ public class AboutFrame extends JInternalFrame {
 		JTextArea txtrJavaccjavajavaJava = new JTextArea();
 		txtrJavaccjavajavaJava.setBackground(Color.PINK);
 		txtrJavaccjavajavaJava.setForeground(Color.RED);
-		txtrJavaccjavajavaJava.setText("      Java是一门面向对象编程语言，不仅吸收了C++语言\r\n的各种优点，还摒弃了C++里难以理解的多继承、指针等\r\n概念，因此Java语言具有功能强大和简单易用两个特征\r\n      Java语言作为静态面向对象编程语言的代表，极好\r\n地实现了面向对象理论，允许程序员以优雅的思维方式进\r\n行复杂的编程 。\r\n      Java具有简单性、面向对象、分布式、健壮性、安\r\n全性、平台独立与可移植性、多线程、动态性等特点 。\r\nJava可以编写桌面应用程序、Web应用程序、分布式系\r\n统和嵌入式系统应用程序等 。");
+		txtrJavaccjavajavaJava.setText("关于我们");
 		txtrJavaccjavajavaJava.setFont(new Font("楷体", Font.PLAIN, 20));
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
